93f716e3ec8798f154201c64d79122f0cc95c08b
Romit Choudhary <67177041+romit0226@users.noreply.github.com>
2021-06-28 07:55:03 +0530

    Update Sort 0_1_2.cpp

--- Arrays/Sort 0_1_2.cpp
+++ Arrays/Sort 0_1_2.cpp
@@ -1,3 +1,7 @@
+// First Approach- Sort using stl (NlogN)
+// Second Approach- Using Counting Sort (count 0 1 2) O(N)
+//Thrid Approach- Using Dutch Flag National algo
+
 void sortColors(vector<int>& nums) {
        int lo=0;
         int high=nums.size()-1;
